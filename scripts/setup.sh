83d61637391c13c527af6c743088dfba23865df9
Trent Shumay <trent@iotdesignshop.com>
2023-10-01 08:32:30 -0700

    Initial check in of shell script and repository definitions

---
+++ scripts/setup.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+OFF='\033[0m'
+RED='\033[0;31m'
+GRN='\033[0;32m'
+BLU='\033[0;34m'
+
+BOLD=$(tput bold)
+NORM=$(tput sgr0)
+
+ERR="${RED}${BOLD}"
+RRE="${NORM}${OFF}"
+
+_usage="${BOLD}USAGE: scripts/setup.sh DESIRED_WORKSPACE_PATH${NORM}
+Installs the set of DexHand ROS 2 Packages into a ROS 2 Workspace 
+at the specified path (DESIRED_WORKSPACE_PATH)."
+
+if [ $# -ne 1 ]; then
+    echo -e "${ERR}ERROR: Incorrect number of arguments.${RRE}"
+    echo -e "${_usage}"
+    exit 1
+fi
+
+ROSDISTRO='humble'
+
+# Create a workspace directory
+mkdir -p $1/src
+
+# Clone the repositories to the src path
+vcs import --input config/dexhand.repos $1/src
+
+# Set up ROS 2 Envrionment
+source /opt/ros/$ROSDISTRO/setup.bash
+
+# Install dependencies
+cd $1
+rosdep install -y --from-paths src --ignore-src --rosdistro $ROSDISTRO
+
+# Build the workspace
+colcon build --symlink-install
+
+
